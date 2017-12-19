8936c7c9a8cc433d738732106faf6c928fae3db3
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-18 19:44:39 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,15 +7,27 @@ class Song < ActiveRecord::Base
      self.artist = Artist.find_or_create_by(name: name)
   end
 
+  def genre_name
+    self.try(:genre).try(:name)
+  end
+
   def artist_name
-    self.artist
+    self.try(:artist).try(:name)
   end
 
   def genre_name=(name)
     self.genre = Genre.find_or_create_by(name: name)
   end
 
-  def genre_name
-    self.genre
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
   end
 end
